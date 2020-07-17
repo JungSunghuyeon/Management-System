64384f2ac7aea953bbbf512b475608c72ba73de7
jungsunghuyeon <gugu9604@naver.com>
2020-07-17 12:52:30 +0900

    React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Customer from './components/Customer'
+
+class App extends Component {
+  render() {
+    return (
+      <Customer/>
+    )
+  }
+}
+
+export default App;
