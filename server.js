89b92c0a4393d5e3ec4b7f56de1394c14764a388
jungsunghuyeon <gugu9604@naver.com>
2020-07-17 15:30:22 +0900

    React

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/customers',(req,res) => {
+    res.send([
+        {
+            'id' :1,
+            'image':'https://placeimg.com/64/64/1',
+            'name':'홍길동',
+            'birthday':'961222',
+            'gender':'남자',
+            'job':'학생'
+            },
+            {
+              'id' :2,
+              'image':'https://placeimg.com/64/64/2',
+              'name':'정성현',
+              'birthday':'960402',
+              'gender':'남자',
+              'job':'인턴' 
+            },
+            {
+            'id' :3,
+            'image':'https://placeimg.com/64/64/3',
+            'name':'김덕배',
+            'birthday':'990821',
+            'gender':'남자',
+            'job':'장군'
+        }
+    ]);
+});
